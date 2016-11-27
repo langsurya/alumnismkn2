10b18e1e44e527c1c0318122d72b273a25f766c3
er1an9 <sun_eagle@ymail.com>
2016-11-27 22:58:33 +0700

    add file

--- media.php
+++ media.php
@@ -6,34 +6,14 @@ switch ($module) {
 	case 'siswa':
 		include_once 'siswa.php';
 		break;
-
-	case 'siswa_input':
-		include_once 'siswa_input.php';
-		break;
-	case 'siswa_edit':
-		include_once 'siswa_edit.php';
-		break;
+	
 	case 'siswa_tampil':
 		include_once 'siswa_tampil.php';
 		break;
-	case 'siswa_search':
-		include_once 'siswa_search.php';
+	case 'login':
+		include_once 'login.php';
 		break;
 
-	case 'jurusan':
-		include_once 'jurusan.php';
-		break;
-	case 'jurusan_input':
-		include_once 'jurusan_input.php';
-		break;
-	case 'jurusan_edit':
-		include_once 'jurusan_edit.php';
-		break;
-
-	case 'keluar':
-		include_once 'keluar.php';
-		break;
-	
 	default:
 		include_once 'home.php';
 		break;
